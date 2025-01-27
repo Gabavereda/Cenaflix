af86930137be78b837cf8a5679432a4603865258
Glauber Vereda <gabavereda@gmail.com>
2025-01-27 18:46:26 -0300

    UPDATES

--- src/main/java/br/com/cenaflix/cenaflix/Cenaflix.java
+++ src/main/java/br/com/cenaflix/cenaflix/Cenaflix.java
@@ -3,14 +3,11 @@ package br.com.cenaflix.cenaflix;
 import br.com.cenaflix.gui.TelaLogin;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author glaub
- */
 public class Cenaflix {
 
     public static void main(String[] args) {
-
+        
+        // INFO PARA lOGIN
         JOptionPane.showMessageDialog(null, "Para validar usuarios\nLogin:user1 Senha:123\nLogin:user2 Senha:123\nLogin:user3 Senha:123 ", "Atenção", 0);
 
         TelaLogin telaLogin = new TelaLogin();
